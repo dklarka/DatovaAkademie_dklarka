ed912b6a978da4e3bcded8ba9a00aff5f0c746c1
dklarka <dklarka@gmail.com>
2024-12-07 17:08:58 +0100

    Otázka 5 + tabulka secondary

---
+++ t_Klara_Dvorakova_project_SQL_secondary_final.sql
@@ -0,0 +1,12 @@
+-- Tabulka s obecnými informacemi o zemích světa
+SELECT * FROM countries AS c;
+
+-- Tabulka s daty o ekonomice států
+SELECT * FROM economies AS e ;
+
+
+SELECT 
+*
+	 c.country 
+	,c.
+FROM countries as c 
