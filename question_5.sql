063612e12b7971e758ebcfcdf53a812c855d3c30
dklarka <dklarka@gmail.com>
2024-12-17 13:31:57 +0100

    Začátek otázky 5

--- question_5.sql
+++ question_5.sql
@@ -1,4 +1,19 @@
 -- 5) Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
 
 SELECT * 
-FROM t_Klara_Dvorakova_project_SQL_primary_final as p;
+FROM t_Klara_Dvorakova_project_SQL_primary_final AS p;
+
+SELECT * 
+FROM t_Klara_Dvorakova_project_SQL_secondary_final AS c;
+
+SELECT 
+	 p.`year` 
+	,AVG(p.wage)
+	,AVG(p.FoodPrice)
+	,p.category 
+-- SELECT *
+FROM t_Klara_Dvorakova_project_SQL_primary_final AS p
+LEFT JOIN t_Klara_Dvorakova_project_SQL_secondary_final AS c ON p.`year` = c.`year`
+WHERE 1=1
+AND c.GDP IS NOT NULL
+GROUP BY p.`year` , p.category ;
